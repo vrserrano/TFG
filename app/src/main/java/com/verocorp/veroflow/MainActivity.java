934dc0a4c5695793998f32fa82369bb64db300e1
Veronica <veronicarodriguezserrano@gmail.com>
2020-04-24 21:29:25 +0200

    Reproductor básico

--- app/src/main/java/com/verocorp/veroflow/MainActivity.java
+++ app/src/main/java/com/verocorp/veroflow/MainActivity.java
@@ -14,16 +14,17 @@ import android.widget.Toast;
 
 import java.io.IOException;
 
+import static com.verocorp.veroflow.R.drawable.astronomia;
+
 public class MainActivity extends AppCompatActivity {
 
-    MediaPlayer mediaPlayer;
     ImageButton playPauseCancion;
     ImageButton siguienteCancion;
     ImageButton anteriorCancion;
-    ImageView  caratula;
+    ImageView caratula;
     int posicion = 0;
 
-    MediaPlayer cancion [] = new MediaPlayer[2];
+    MediaPlayer cancion[] = new MediaPlayer[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +36,16 @@ public class MainActivity extends AppCompatActivity {
         anteriorCancion = findViewById(R.id.anteriorCancion);
         caratula = findViewById(R.id.caratula1);
 
-        cancion [0] = MediaPlayer.create( this, R.raw.song1);
-        cancion [1] = MediaPlayer.create( this, R.raw.song2);
+        cancion[0] = MediaPlayer.create(this, R.raw.song1);
+        cancion[1] = MediaPlayer.create(this, R.raw.song2);
 
         //Método para el botón playPause
-        public void onClick (View view) {
-                if (cancion[posicion].isPlaying()){
-                     cancion[posicion].pause();
-                     playPauseCancion.setBackgroundResource(R.drawable.ic_play_arrow_black_24dp);
+        playPauseCancion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cancion[posicion].isPlaying()) {
+                    cancion[posicion].pause();
+                    playPauseCancion.setImageResource(R.drawable.ic_play_arrow_black_24dp);
                     Toast.makeText(MainActivity.this, "Pause", Toast.LENGTH_SHORT).show();
                 } else {
                     cancion[posicion].start();
@@ -50,29 +53,69 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
                 }
             }
-            public void siguienteCancion (View view) {
-                if (posicion < cancion.length -1) {
+        });
 
+        siguienteCancion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (posicion < cancion.length - 1) {
                     if (cancion[posicion].isPlaying()) {
                         cancion[posicion].pause();
                         posicion++;
                         cancion[posicion].start();
 
-                        if (posicion == 0 ) {
-                            caratula.setImageResource(R.drawable.lola1);
-                        } else if (posicion ==1){
-                            caratula.setImageResource(R.drawable.pajaros);
+                        if (posicion == 0) {
+                            caratula.setImageResource(astronomia);
+                        } else if (posicion == 1) {
+                            caratula.setImageResource(R.drawable.picture);
                         }
                     } else {
                         posicion++;
 
                         if (posicion == 0) {
-                            caratula.setImageResource(R.drawable.lola1);
-                        } else if (posicion ==1){
-                            caratula.setImageResource(R.drawable.pajaros);
+                            caratula.setImageResource(astronomia);
+                        } else if (posicion == 1) {
+                            caratula.setImageResource(R.drawable.picture);
                         }
                     }
                 } else {
                     Toast.makeText(MainActivity.this, "Fin lista canciones", Toast.LENGTH_SHORT).show();
                 }
-        };
+            }
+        });
+
+        anteriorCancion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (posicion >= 1) {
+
+                    if (cancion[posicion].isPlaying()) {
+                        cancion[posicion].pause();
+
+                        playPauseCancion = findViewById(R.id.playCancion);
+                        siguienteCancion = findViewById(R.id.siguienteCancion);
+                        anteriorCancion = findViewById(R.id.anteriorCancion);
+                        posicion--;
+
+                        if (posicion == 0) {
+                            caratula.setImageResource(astronomia);
+                        } else if (posicion == 1) {
+                            caratula.setImageResource(R.drawable.picture);
+                        }
+                        cancion[posicion].start();
+                    } else {
+                        posicion--;
+
+                        if (posicion == 0) {
+                            caratula.setImageResource(astronomia);
+                        } else if (posicion == 1) {
+                            caratula.setImageResource(R.drawable.picture);
+                        }
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Fin lista canciones", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
